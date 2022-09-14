d2f4ebe7fb59ca9519591e387c212fd46c9e8813
NotStoneSeok <kimsw0683@handong.ac.kr>
2022-09-15 00:26:40 +0900

    first_Commit

---
+++ hw1/hw1.cpp
@@ -0,0 +1,34 @@
+#include "cv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat frame;
+    int fps;
+    int delay;
+    int total;
+    VideoCapture cap;
+
+    if (cap.open("background.mp4") == 0)
+    {
+        cout << "no such file!" << endl;
+        waitKey(0);
+    }
+    fps = cap.get(CAP_PROP_FPS);
+    total = cap.get(CAP_PROP_FRAME_COUNT)
+    delay = 1000 / fps;
+    while (1)
+    {
+        cap >> frame;
+        cout << "frames: " << fps*cap.get(CAP_PROP_POS_MSEC)/1000 << " / " << total << endl;
+        if (cap.get(CAP_PROP_POS_MSEC) >=3000) //
+        {
+            break;
+        }
+        imshow("video", frame);
+        waitKey(delay);
+    }
+}
---
+++ hw2/hw2.cpp
@@ -0,0 +1,37 @@
+#include "cv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat image = imread("lena.png", 0);
+    Mat negative_img = image.clone();
+
+    unsigned char pix[256];
+    float gamma = 10.0;
+
+    for (int i = 0; i < 256; i++)
+    {
+        pix[i] = saturate_cast<uchar>(pow((float)(i / 255.0), gamma) * 255.0f);
+    }
+    for (int j = 0; j < image.rows; j++)
+        for (int i = 0; i < image.cols; i++)
+        {
+            if (negative_img.at<uchar>(j, i) < 127)
+            {
+                negative_img.at<uchar>(j, i) = 255 - image.at<uchar>(j, i);
+            }
+            else
+            {
+                negative_img.at<uchar>(j, i) = pix[negative_img.at<uchar>(j, i)];
+            }
+        }
+    imshow("Input image", image);
+    rotate(negative_img, negative_img, ROTATE_90_COUNTERCLOCKWISE);
+    imshow("Negative transformation", negative_img);
+    waitKey(0);
+
+    return 0;
+}
