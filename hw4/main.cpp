976bcc7ef178ef9beffd6b0627c27ded2b5abe73
NotStoneSeok <98035984+isaccKim@users.noreply.github.com>
2022-09-23 10:27:27 +0900

    histogram equalization
    Finished
    CalcHist 사용법 익힌날..

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -8,7 +8,7 @@ Mat drawHistogram(Mat src)
 {
     Mat hist,histImage;// establish the number of bins
     int i, hist_w, hist_h, bin_w, histSize;
-    float range[] = {0, 255};
+    float range[] = {0, 256};
     const float *histRange = {range};
     hist_w = 512;
     hist_h = 512;
@@ -17,22 +17,40 @@ Mat drawHistogram(Mat src)
     histImage = Mat(hist_h, hist_w, CV_8UC3, Scalar(255, 255, 255));
     calcHist(&src, 1, 0, Mat(), hist, 1, &histSize, &histRange);
     normalize(hist, hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-
     for (i = 0; i < histSize; i++)
     {
         rectangle(histImage, Point(bin_w * i, hist_h), Point(bin_w * i + hist_w / histSize, hist_h - cvRound(hist.at<float>(i))), Scalar(0, 0, 0), -1);
+
     }
 
     return histImage;
 }
 
+Mat putTextValue(Mat src)
+{
+    Mat hist,histImage;// establish the number of bins
+    int i, hist_w, hist_h, bin_w, histSize;
+    float range[] = {0, 256};
+    const float *histRange = {range};
+    histSize = 8;
+    calcHist(&src, 1, 0, Mat(), hist, 1, &histSize, &histRange);
+        
+    for (i = 0; i < histSize; i++){
+        float pixel = src.rows * src.cols;
+        putText(src,format("bin %d : %f",i+1,hist.at<float>(i)/pixel),Point(40,40+i*40),FONT_HERSHEY_COMPLEX,0.5,Scalar(0,20,2), 1);
+    }
+    return histImage;
+}
+
 int main()
 {
+
     Mat image;
     Mat hist_equalized_image;
     Mat hist_graph;
     Mat hist_equalized_graph;
     image = imread("Moon.png", 0);
+    
     if (!image.data)
         exit(1);                               // Check image
     equalizeHist(image, hist_equalized_image); // histogram equlization
@@ -40,11 +58,12 @@ int main()
 
     hist_equalized_graph = drawHistogram(hist_equalized_image);
 
-for(int i=0; i<8; i++){
-            putText(image,format("bin %d : %d",i+1,1),Point(40,40+i*40),FONT_HERSHEY_COMPLEX,0.5,Scalar(0,200,200),1);
-    }
+    putTextValue(image);
+    
     imshow("before", image);
 
+    putTextValue(hist_equalized_image);
+    
     imshow("after",
            hist_equalized_image);
 
