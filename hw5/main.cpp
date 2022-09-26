0fcba33285de27d042ef600d3d9368a82118714a
NotStoneSeok <98035984+isaccKim@users.noreply.github.com>
2022-09-26 09:55:15 +0900

    hw5 Updated
    ing..

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -4,16 +4,59 @@
 using namespace cv;
 using namespace std;
 
+Mat drawLines(Mat left_src,Mat right_src, Mat result){
+        vector<Vec2f> lines;
+        float rho=0, theta=0, a, b, x0, y0;
+        Point p1, p2;
+        // HoughLines(left_src, lines,1,CV_PI/180,150,0,0,30*CV_PI/180,60*CV_PI/180);
+        HoughLines(left_src, lines,1,CV_PI/180,150,0,0,30*CV_PI/180,60*CV_PI/180);
+        for (int i = 0; i < lines.size(); i++)
+        {
+            rho += lines[i][0];
+            theta += lines[i][1];   
+        }
+        rho /=lines.size();
+        theta /=lines.size();
+
+            a = cos(theta);
+            b = sin(theta);
+            x0 = a * rho;
+            y0 = b * rho;
+            p1 = Point(cvRound(x0 + 1000 * (-b)), cvRound(y0 + 1000 * a));
+            p2 = Point(cvRound(x0 - 1000 * (-b)), cvRound(y0 - 1000 * a));
+            line(result, p1, p2, Scalar(0, 0, 255), 3, 0);
+
+        HoughLines(right_src, lines,1,CV_PI/180,150,0,0,120*CV_PI/180,150*CV_PI/180);
+            rho =0;
+            theta = 0;
+            for (int i = 0; i < lines.size(); i++)
+        {
+            rho += lines[i][0];
+            theta += lines[i][1];   
+        }
+        rho /=lines.size();
+        theta /=lines.size();
+
+            a = cos(theta);
+            b = sin(theta);
+            x0 = a * rho;
+            y0 = b * rho;
+            p1 = Point(cvRound(x0 + 1000 * (-b)), cvRound(y0 + 1000 * a));
+            p2 = Point(cvRound(x0 - 1000 * (-b)), cvRound(y0 - 1000 * a));
+            line(result, p1, p2, Scalar(0, 0, 255), 3, 0);
+            return result;
+}
+
 int main()
 {
-    Mat frame, canny_left, canny_right,gray, blur, canny;
+    Mat frame, canny_left, canny_right,gray,blurIm, canny;
     int fps;
     int delay;
     int total;
+
     Mat result;
     Point p1, p2;
-    vector<Vec2f> lines;
-    float rho, theta, a, b, x0, y0;
+    
     VideoCapture cap; // 카메라 또는 비디오 파일로 부터 프레임을 읽는데 필요한 정보
 
     if (cap.open("Road.mp4") == 0)
@@ -24,7 +67,6 @@ int main()
     fps = cap.get(CAP_PROP_FPS); // 초당 프레임 수
 
     delay = 1000 / fps;
-
     while (1)
     {   
         cap >> frame;
@@ -32,33 +74,19 @@ int main()
         {
             break;
         }
-
         cvtColor(frame, gray, CV_BGR2GRAY );
-        
-        GaussianBlur(gray,blur,Size(5,5),5,5,BORDER_DEFAULT);
-        
-        Canny(blur,canny,10,60,3);
 
-        canny_left = canny(Range(400, 600), Range(200, 600));
-        canny_right = canny(Range(400, 600), Range(600, 1000));
+        blur(gray,blurIm,Size(5,5));
+        Canny(blurIm,canny,10,60,3);
 
-        // HoughLines(canny, lines, 1, CV_PI / 180, 150,0,CV_PI);
+        canny_left = canny;
+        //yhucanny(Range(400, 600), Range(200, 600));
+        //canny_right = canny(Range(400, 600), Range(600, 1000));
+        canny_right = canny;
+        frame = drawLines(canny_left,canny_right,frame);
         
-        // for (int i = 0; i < lines.size(); i++)
-        // {
-        //     rho = lines[i][0];
-        //     theta = lines[i][1];
-        //     a = cos(theta);
-        //     b = sin(theta);
-        //     x0 = a * rho;
-        //     y0 = b * rho;
-        //     p1 = Point(cvRound(x0 + 1000 * (-b)), cvRound(y0 + 1000 * a));
-        //     p2 = Point(cvRound(x0 - 1000 * (-b)), cvRound(y0 - 1000 * a));
-        //     line(result, p1, p2, Scalar(0, 0, 255), 3, 8);
-        // }
-
-        imshow("video", frame);
-        moveWindow("Left Canny", 200, 0); 
+        imshow("video",frame);
+        moveWindow("Left Canny", 200, 0);
         imshow("Left Canny", canny_left);
         moveWindow("Right Canny", 600, 0);
         imshow("Right Canny", canny_right);
