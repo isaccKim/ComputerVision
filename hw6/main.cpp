c0f9bcb2651a4743aa12148bb2efca7501dced2e
NotStoneSeok <98035984+isaccKim@users.noreply.github.com>
2022-10-04 15:45:54 +0900

    hw6,Project1 Updated
    hw6
    - inrange 함수 사용안하고 adaptive threshold 사용
    Project1
    - 프로젝트1 시작 하기 위한 파일 정리

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -26,15 +26,12 @@ int main()
     cvtColor(a_image,a_image,CV_BGR2GRAY);
     cvtColor(a1_image,a1_image,CV_BGR2GRAY);
 
-    imshow("temp",a_image);    
     threshold(f_image,f_dst,160, 255, CV_THRESH_BINARY);
-    
-    //GaussianBlur(a_image, a_image, Size(3,3), 0);
     //inRange(a_image,Scalar(160),Scalar(225),a_dst);
     
-    adaptiveThreshold(a_image,a_dst, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C,THRESH_BINARY,7,8); //줄무늬 격자가 있는 이미지 => 가우시안블러처리
+    adaptiveThreshold(a_image,a_dst, 255, CV_ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY,7,8); //줄무늬 격자가 있는 이미지 => 가우시안블러처리
     
-    adaptiveThreshold(a1_image,a1_dst, 255, ADAPTIVE_THRESH_GAUSSIAN_C,THRESH_BINARY,7,8); //줄무늬 격자가 있는 이미지 => 가우시안블러처리
+    adaptiveThreshold(a1_image,a1_dst, 255, ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY,7,8); //
     
 
     imshow("finger_print.png",f_dst);
