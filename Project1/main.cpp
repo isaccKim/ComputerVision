be87baf31cc45e000f316afe370a20f790cfe66d
NotStoneSeok <98035984+isaccKim@users.noreply.github.com>
2022-10-06 16:01:39 +0900

    Project1 Updated
    키 입력 구현

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,64 +3,34 @@
 
 using namespace cv;
 using namespace std;
+/*
+• Develop a program which edits the input video
+• Read ‘video.mp4’. Depending on the input of a user, your program should do the following operations
+• Negative transformation on the user input ‘n’ • Gamma transformation with the value of gamma as 2.5 on the user input ‘g’ • Histogram equalization on the user input ‘h’ • Color slicing on the user input ‘s’ • Hue value: 9<hue<23
+• Color conversion on the user input ‘c’ • Increase Hue value by 50
+• For hue values bigger than 129, subtract 129 instead
+• Average filtering on the user input ‘a’ • Use “blur” function with mask size as 9X9
+• White balancing by using gray world assumption on the user input ‘w’ • Reset the image on the user input ‘r’
 
-Mat drawLines(Mat left_src, Mat right_src, Mat result)
-{
-    vector<Vec2f> lines;
-    float rho = 0, theta = 0, a, b, x0, y0;
-    Point p1, p2;
-    HoughLines(left_src, lines, 1, CV_PI / 180, 100, 0, 0, 30 * CV_PI / 180, 60 * CV_PI / 180); 
-    // HoughLines : 일반 평면에 있던 점들을 다른 좌표평면에 직선으로 나타내고 그값을 lines애 저장
-    for (int i = 0; i < lines.size(); i++)
-    {
-        rho += lines[i][0];
-        theta += lines[i][1];
-    }
-    rho /= lines.size();  
-    theta /= lines.size();
+Your program should display one window ‘video’
+• Depending on the input of the user, contents in the window should be changed
+• For color conversion, use CV_BGR2HSV and CV_HSV2BGR
+• Use waitKey for user interaction
+• waitKey returns the code of the pressed key or -1 if no key was pressed
+before the specified time had elapsed. • ESC  27
 
-    a = cos(theta);
-    b = sin(theta);
-    x0 = a * rho; // 평면 옮기는 작업
-    y0 = b * rho;
-    p1 = Point(cvRound(x0 + 1000 * (-b)) + 200, cvRound(y0 + 1000 * a) + 400);
-    p2 = Point(cvRound(x0 - 1000 * (-b)) + 200, cvRound(y0 - 1000 * a) + 400);
-    // ROI를 하며 바뀐 중심측
-    if(p1 != Point(200,400) && p2 != Point(200,400)){
-        line(result, p1, p2, Scalar(0, 0, 255), 4, 0); // x,y,좌표가 없을 경우 200,400좌표에 점이 생기는 것을 막기위한 if문
-    } 
-
-    HoughLines(right_src, lines, 1, CV_PI / 180, 100, 0, 0, 120 * CV_PI / 180, 150 * CV_PI / 180);
-    rho = 0;
-    theta = 0;
-    for (int i = 0; i < lines.size(); i++)
-    {
-        rho += lines[i][0];
-        theta += lines[i][1];
-    }
-    rho /= lines.size(); 
-    theta /= lines.size();
-
-    a = cos(theta);
-    b = sin(theta);
-    x0 = a * rho;
-    y0 = b * rho;
-    p1 = Point(cvRound(x0 + 1000 * (-b)) + 600, cvRound(y0 + 1000 * a) + 400);
-    p2 = Point(cvRound(x0 - 1000 * (-b)) + 600, cvRound(y0 - 1000 * a) + 400);
-    if(p1 != Point(600,400) && p2 != Point(600,400)){
-        line(result, p1, p2, Scalar(0, 0, 255), 4, 0);
-    }
-    return result;
-}
+*/
 
 int main()
 {
-    Mat frame, canny_left, canny_right, gray, blurIm, canny;
+    Mat frame;
+
     int fps;
-    int delay;
-
-
+    int delay, temp;
+    int input;
     VideoCapture cap; // 카메라 또는 비디오 파일로 부터 프레임을 읽는데 필요한 정보
+    float gamma = 0.5;
+    unsigned char pix[256];
 
     if (cap.open("Road.mp4") == 0)
     {
@@ -70,27 +40,65 @@ int main()
     fps = cap.get(CAP_PROP_FPS); // 초당 프레임 수
 
     delay = 1000 / fps;
+
     while (1)
     {
         cap >> frame;
+
         if (cap.get(CAP_PROP_POS_MSEC) >= 20000)
         {
             break;
         }
-        cvtColor(frame, gray, CV_BGR2GRAY);
 
-        blur(gray, blurIm, Size(5, 5));
-        Canny(blurIm, canny, 10, 60, 3);
+        temp = waitKey(delay);
+        cout << temp << endl;
 
-        canny_left = canny(Range(400, 600), Range(200, 600));
-        canny_right = canny(Range(400, 600), Range(600, 1000));
-        frame = drawLines(canny_left, canny_right, frame);
+        if (temp != -1)
+        {
+            input = temp;
+        }
 
+        if (input == 110) // n
+        {
+            for (int j = 0; j < frame.rows; j++)
+                for (int i = 0; i < frame.cols * 3; i++)
+                {
+                    frame.at<uchar>(j, i) = 255 - frame.at<uchar>(j, i);
+                }
+        }
+
+        if (input == 103)//g
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                pix[i] = saturate_cast<uchar>(pow((float)(i / 255.0), gamma) * 255.0f);
+            }
+            for (int j = 0; j < frame.rows; j++)
+            {
+                for (int i = 0; i < frame.cols * 3; i++)
+                {
+                    frame.at<uchar>(j, i) = pix[frame.at<uchar>(j, i)];
+                }
+            }
+        }
+        if (input ==  104){ //h
+            cvtColor(frame, frame, CV_BGR2GRAY);
+            equalizeHist(frame, frame); // histogram equlization
+        }
+        if (input == 115){ //s
+        }
+        if(input == 99){ //c
+
+        }
+        if(input == 97){//a
+            blur(frame,frame,Size(9,9));
+        }
+        if(input == 119){ //w
+
+        }
+        if(input == 114){ //r
+        
+        }
         imshow("video", frame);
-        moveWindow("Left Canny", 200, 0);
-        imshow("Left Canny", canny_left);
-        moveWindow("Right Canny", 600, 0);
-        imshow("Right Canny", canny_right);
-        waitKey(delay);
     }
 }
